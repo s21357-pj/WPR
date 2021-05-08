703dd79e937cfc11a9a2b1af20f4128b8cb681cf
s21357 <s21357@pjwstk.edu.pl>
2021-05-08 22:51:39 +0100

    fix

--- 3/2.php
+++ 3/2.php
@@ -10,12 +10,9 @@
   <input type="submit" value="Submit">
 </form> 
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 if(isset($_POST["a"]) && isset($_POST["b"])) {
 	try {
-		if(file_put_contents("2.csv", $_POST["a"]." ".$_POST["b"]."\b", FILE_APPEND | LOCK_EX) !== false) {
+		if(file_put_contents("2.csv", $_POST["a"]." ".$_POST["b"]."\r\n", FILE_APPEND | LOCK_EX) !== false) {
 			echo("Information has been saved");
 		}
 	} catch (Exception $e) {
